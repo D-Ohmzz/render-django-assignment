52f71f2f63d79738adc517d8b4f9e0adde165ec1
D-Ohmzz <douglasomega13@gmail.com>
2024-07-12 08:32:27 +0300

    set environment variables on sms_trial

--- api/sms_trial.py
+++ api/sms_trial.py
@@ -1,8 +1,9 @@
 import africastalking
+import os
 
 # Initialize SDK
-username = "sandbox"    # use 'sandbox' for development in the test environment
-api_key = "atsk_ed34c399ef839b5af0ead66cb52cba3e6e5e89dda614ceec9933969d3ff99c8db10aa46c"      # use your sandbox app API key for development in the test environment
+username = os.environ.get('AT_USERNAME')    # use 'sandbox' for development in the test environment
+api_key = os.environ.get('AT_API_KEY')      # use your sandbox app API key for development in the test environment
 africastalking.initialize(username, api_key)
 
 #Initialize sms service
@@ -11,11 +12,11 @@ sms = africastalking.SMS
 #Function for sending sms alert
 def send_sms_alert(phone_number, message):
     try:
-        sender = "1739"
+        sender = os.environ.get('AT_SENDER')
         response = sms.send(message, [phone_number], sender)
     except Exception as e:
         print("Error sending message")
 
 message = f"Order received: Pens amounting to: 350 at 2024-06-21 15:00."
-phone_number = "+254798889503"
+phone_number = "+2547xxxxxxxx"
 send_sms_alert(phone_number, message)
